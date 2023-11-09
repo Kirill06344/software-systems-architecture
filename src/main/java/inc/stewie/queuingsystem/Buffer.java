62daa7981de5e713cd19d8e953e64da2af767ddf
stewie <kirill12093@mail.ru>
2023-11-09 18:32:27 +0300

    feat: add naked Buffer

--- src/main/java/inc/stewie/queuingsystem/Buffer.java
+++ src/main/java/inc/stewie/queuingsystem/Buffer.java
@@ -1,7 +1,26 @@
 package inc.stewie.queuingsystem;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Buffer {
+
+    private final Request [] requests;
+    private int size;
+
+    private int lastIndex = -1;
+
+    public Buffer(@Value("${model.vars.requests") int capacity) {
+        requests = new Request[capacity];
+    }
+
+    public void addRequest(Request request) {
+        
+    }
+
+    public boolean isFilled() {
+        return size == requests.length;
+    }
+
 }
