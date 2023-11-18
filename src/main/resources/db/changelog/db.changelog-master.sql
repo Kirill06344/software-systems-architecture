989859a3d2982552001d1400f5ecbddab700a027
stewie <kirill12093@mail.ru>
2023-11-19 00:59:08 +0300

    feat: add schema for table

--- src/main/resources/db/changelog/db.changelog-master.sql
+++ src/main/resources/db/changelog/db.changelog-master.sql
@@ -1,6 +1,24 @@
 --liquibase formatted sql
 
 --changeset stewie:1
-create table events (
-    id int primary key
+create table sources (
+    id int primary key,
+    time real not null default 0.0,
+    request_amount int not null default 0,
+    refuse_amount int not null default 0
+);
+
+create type device_state as enum('FREE', 'BUSY');
+
+create table devices (
+  id int primary key,
+  state device_state not null default 'FREE',
+  request_amount int not null default 0
+);
+
+create table buffer (
+    position int primary key,
+    time real not null default 0.0,
+    source_id int references sources(id),
+    request_number int unique
 )
